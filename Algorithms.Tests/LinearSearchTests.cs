b1cbb1ecf38406fff24c21bb391872b9a94f2183
Kim Diep <kim.diep.techno@gmail.com>
2022-04-21 14:36:59 +0100

    Adds LinearSearch unit test and skeleton class with GetSearchTimeForContact method

---
+++ Algorithms.Tests/LinearSearchTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using Algorithms.Models;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Algorithms.Tests;
+
+public class LinearSearchTests
+{
+    [Test]
+    public void Should_Record_The_Search_Time_For_Worst_Case_With_A_Match()
+    {
+        // Arrange
+        var contacts = new List<Contact>
+        {
+            new("Aaron", "Smith", "0207123434"),
+            new("Ben", "Smith", "0207123434"),
+            new("Jane", "Smith", "0207123456"),
+            new("Mike", "Smith", "0207123456"),
+            new("Pamela", "Smith", "0207123456"),
+            new("Xander", "Smith", "0207123458"),
+            new("Zach", "Smith", "0207123458"),
+        };
+
+        // Act
+        int searchTime = LinearSearch.GetSearchTimeForContact(contacts, "Xander");
+
+        // Assert
+        // Assuming it takes 200 ms to search per person
+        searchTime.Should().Be(600);
+    }
+
+    [Test]
+    public void Should_Record_The_Search_Time_For_Worst_Case_Without_A_Match()
+    {
+        // Arrange
+        var contacts = new List<Contact>
+        {
+            new("Aaron", "Smith", "0207123434"),
+            new("Ben", "Smith", "0207123434"),
+            new("Jane", "Smith", "0207123456"),
+            new("Mike", "Smith", "0207123456"),
+            new("Pamela", "Smith", "0207123456"),
+            new("Xander", "Smith", "0207123458"),
+            new("Zach", "Smith", "0207123458"),
+        };
+
+        // Act
+        int searchTime = LinearSearch.GetSearchTimeForContact(contacts, "June");
+
+        // Assert
+        // Assuming it takes 200 ms to search per person
+        searchTime.Should().Be(800);
+    }
+}
---
+++ Algorithms/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Algorithms.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string TelephoneNumber { get; private set; }
+
+        public Contact(string firstName, string lastName, string telephoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            TelephoneNumber = telephoneNumber;
+        }
+    }
+}
+
